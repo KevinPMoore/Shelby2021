0485c0df90113adb605ca6d2d98c89914d3c1b0b
Kevin Moore <moore.kevin.p@gmail.com>
2021-04-26 12:03:29 -0400

    initial descriptions written

--- src/index.js
+++ src/index.js
@@ -1,59 +1,86 @@
 'use strict';
 
 function initialContent() {
+  $('h1').toggleClass('hidden');
   setTimeout(function () {
     $('p.mainText').append('What type of content do you want to see?');
   }, 3000);
   setTimeout(function () {
     $('.contentContainer').append(
-      `<button class='dancing'>Rude Dancing</button> 
+      `<button class='dance'>Cool Dances</button> 
       <button class='photo'>Cute Photos</>`);
-  }, 5000);
+  }, 6000);
 }
 
 function bindDancingClick() {
-  $('.contentContainer').on('click', '.dancing', event => {
-    console.log('bind dancing click ran');
+  $('.contentContainer').on('click', '.dance', () => {
     $('h1').addClass('hidden');
     $('.mainText').empty();
     $('.contentContainer').html(
       `<div class='imgPlaceholder'></div>
       <p class='dancingDescription'>
-        This is some text about dancing
+        Forced to perform at a mysterious dance party, the spotlight shines on you! </br>
+        Will your moves steal the show or will you burn out in the starlight? </br>
+        ITS SHOWTIME!
       </p>
-      <button class='back'>Go Back</button>`
+      <button class='confirm'>I wanna dance!</button>
+      <button class='back'>I'm not sure</button>`
     );
   });
 }
 
 function bindPhotoClick() {
-  $('.contentContainer').on('click', '.photo', event => {
-    console.log('bind photo click ran');
-    $('h1').addClass('hidden');
-    $('p').empty();
+  $('.contentContainer').on('click', '.photo', () => {
+    $('h1').toggleClass('hidden');
+    $('.mainText').empty();
     $('.contentContainer').html(
       `<div class='imgPlaceholder'></div>
       <p class='photoDescription'>
-        This is some text about photos
+        See a whole new world through the lens of your camera! </br>
+        Photograph the flora and fauna of dense jungles and vast deserts. </br>
+        You might even unravel the mysteries of the Illumina phenomenon!
       </p>
+      <button class='confirm'>My camera is ready!</button>
       <button class='back'>Go Back</button>`
     );
   });
 }
 
 function bindGoBack() {
-  $('.contentContainer').on('click', '.back', event => {
+  $('.contentContainer').on('click', '.back', () => {
     $('.contentContainer').empty();
     initialContent();
   });
 }
 
+function bindConfirm() {
+  $('.contentContainer').on('click', '.confirm', () => {
+    handleConfirmClick($('.contentContainer').find('p').attr('class'));
+  });
+}
+
+function handleConfirmClick(choice) {
+  if (choice === 'dancingDescription') {
+    renderDance();
+  } else if (choice ==='photoDescription') {
+    renderPhoto();
+  }
+}
+
+function renderDance() {
+  console.log('renderDance ran');
+}
+
+function renderPhoto() {
+  console.log('renderPhoto ran');
+
+}
 
 function bindAllButtons() {
-  console.log('binding all buttons!');
   bindDancingClick();
   bindPhotoClick();
   bindGoBack();
+  bindConfirm();
 }
 
 function main() {
