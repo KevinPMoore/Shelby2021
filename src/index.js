74a0ec59bff106ac8361b73685ad2d24d5637e6b
Kevin Moore <moore.kevin.p@gmail.com>
2021-04-26 14:17:48 -0400

    refactored and styled initial choice views

--- src/index.js
+++ src/index.js
@@ -16,46 +16,54 @@ function initialContent() {
 function bindDancingClick() {
   $('.contentContainer').on('click', '.danceButton', () => {
     $('h1').addClass('hidden');
-    $('.mainText').empty();
-    $('.contentContainer').html(
-      `<p class='dancing'>
-        Forced to perform at a mysterious dance party, the spotlight shines on you!
-      </p>
-      <p class='dancing2'>
-        Will your moves steal the show or will you burn out in the starlight?
-      <p class='dancing3'>
-        ITS SHOWTIME!
-      </p>
-      <button class='confirm'>I wanna dance!</button>
-      <button class='back'>I'm not sure</button>`
-    );
+    $('.contentContainer').empty();
+    setTimeout(function () {
+      $('.contentContainer').html('<p class=\'dancing\'>Forced to perform at a mysterious dance party, the spotlight shines on you!</p>');
+    }, 1000);
+    setTimeout(function() {
+      $('.contentContainer').append('<p class=\'dancing\'>Will your moves steal their hearts or will you burn out in the starlight?</p>');
+    }, 4000);
+    setTimeout(function() {
+      $('.contentContainer').append('<p class=\'dancing\'>Its showtime!</p>');
+    }, 7000);
+    setTimeout(function() {
+      $('.contentContainer').append(
+        `<button class='confirm'>I wanna dance!</button>
+        <button class='back'>I'm not sure</button>`);
+    }, 9000);
   });
 }
 
 function bindPhotoClick() {
   $('.contentContainer').on('click', '.photoButton', () => {
-    $('h1').toggleClass('hidden');
-    $('.mainText').empty();
-    $('.contentContainer').html(
-      `<p class='photo'>
-        See a whole new world through the lens of your camera!
-      </p>
-      <p class='photo2'>
-        Photograph the flora and fauna of dense jungles and vast deserts.
-      </p>
-      <p class='photo3'>
-        You might even unravel the mysteries of the Illumina phenomenon!
-      </p>
-      <button class='confirm'>My camera is ready!</button>
-      <button class='back'>Go Back</button>`
-    );
+    $('h1').addClass('hidden');
+    $('.contentContainer').empty();
+    setTimeout(function () {
+      $('.contentContainer').html('<p class=\'photo\'>See a whole new world through the lens of your camera!</p>');
+    }, 1000);
+    setTimeout(function() {
+      $('.contentContainer').append('<p class=\'photo\'>Snapshot the flora and fauna of dense jungles and vast deserts.</p>');
+    }, 4000);
+    setTimeout(function() {
+      $('.contentContainer').append('<p class=\'photo\'>Unravel the mysteries of the Illumina phenomenon.</p>');
+    }, 7000);
+    setTimeout(function() {
+      $('.contentContainer').append(
+        `<button class='confirm'>My camera is ready!</button>
+        <button class='back'>I'm not sure</button>`);
+    }, 9000);
   });
 }
 
 function bindGoBack() {
   $('.contentContainer').on('click', '.back', () => {
-    $('.contentContainer').empty();
-    initialContent();
+    $('h1').toggleClass('hidden');
+    $('.contentContainer').html(
+      `<p class='mainText'>What type of content do you want to see?</p>
+      <p class='mainText'>Choose carefully</p>
+      <button class='danceButton'>Cool Dances</button> 
+      <button class='photoButton'>Cute Photos</>`
+    );
   });
 }
 
